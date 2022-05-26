9dac4a033f4f179e092fd10a695771408fca1ee5
Benx001 <benminm@gmail.com>
2022-05-25 19:51:40 -0500

    Update PCD8544.h

--- libraries/c/PCD8544/PCD8544.h
+++ libraries/c/PCD8544/PCD8544.h
@@ -6,7 +6,7 @@
  Copyright (C) 2010 Limor Fried, Adafruit Industries
  CORTEX-M3 version by Le Dang Dung, 2011 LeeDangDung@gmail.com (tested on LPC1769)
  Raspberry Pi version by Andre Wussow, 2012, desk@binerry.de
- Raspberry Pi Pico version by Benx001, 2022
+ Raspberry Pi Pico version by _Benjamín Morales_, 2022, benminm@gmail.com
 
  Description : PCD8544 LCD library!
 
